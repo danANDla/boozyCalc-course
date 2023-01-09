78eb2b3cc469de8b26ae50704dfb9a7bfe39498e
archdla <d.a.n.202021@gmail.com>
2023-01-09 19:13:15 +0300

    calculating product price

--- boozyfront/src/components/listsViewer/AddProductForm.vue
+++ boozyfront/src/components/listsViewer/AddProductForm.vue
@@ -2,7 +2,7 @@
   <form @submit.prevent>
     <div class="container">
       <div class="error-container" v-if="isError">
-        {{errorText}}
+        {{ errorText }}
       </div>
       <div>
         <my-input
@@ -25,15 +25,31 @@
           choose ingredient
         </drop-down>
       </div>
-      <div>
+      <div class="pricer">
+        <my-input
+            v-model="this.price"
+            type="number"
+            placeholder="rub"
+            v-on:change="updatePrice()"
+        />
+
+        <my-input
+            v-model="this.amount"
+            type="number"
+            placeholder="ml"
+            v-on:change="updatePrice()"
+        />
+
+        <!--        {{this.product.price}}-->
         <my-input
             v-model="this.product.price"
             type="number"
             placeholder="rub/ml"
+            disabled
         />
       </div>
       <div class="btn-container">
-        <rect-button btn-type="purple" @click="submitData"> add </rect-button>
+        <rect-button btn-type="purple" @click="submitData"> add</rect-button>
       </div>
     </div>
   </form>
@@ -43,68 +59,86 @@
 import DropDown from "@/components/UI/DropDown";
 import MyInput from "@/components/UI/MyInput";
 import RectButton from "@/components/UI/RectButton";
+
 export default {
   name: "AddIngredientForm",
   components: {RectButton, MyInput, DropDown},
-  props:{
-    isError:{
+  props: {
+    isError: {
       type: Boolean,
       default: false
     },
-    errorText:{
+    errorText: {
       type: String,
       default: ""
     },
-    ingredients:{
+    ingredients: {
       type: Array,
       default: ""
     },
   },
-  data(){
-    return{
-      product:{
+  data() {
+    return {
+      product: {
         name: '',
         description: '',
         price: undefined,
         ingredientId: -1
-      }
+      },
+      amount: undefined,
+      price: undefined
     }
   },
-  methods:{
-    submitData(){
+  methods: {
+    submitData() {
       this.$emit('submitData', this.product)
     },
-
-  }
+    updatePrice() {
+      if (this.price <= 0) this.price = 0;
+      if (this.amount <= 0) this.amount = 0;
+      if (this.amount !== 0) this.product.price = Math.round(1000 * this.price / this.amount) / 1000;
+    }
+  },
 }
 </script>
 
 <style scoped>
-form{
+form {
   width: 100%;
   padding: 10px;
   display: flex;
   background-color: #3D5E6B;
   justify-content: center;
 }
-.container{
+
+.container {
   width: 100%;
-  display:flex;
+  display: flex;
   flex-direction: column;
 }
-.container div{
+
+.container div {
   padding: 10px;
 }
 
-.btn-container{
+.btn-container {
   display: flex;
   justify-content: end;
   align-items: center;
 }
 
-.error-container{
+.error-container {
   text-align: center;
   font-size: 15px;
   color: #d4aa70;
+}
+
+.pricer {
+  display: flex;
+  gap: 5px;
+}
+
+input:disabled {
+  background-color: white;
 }
 </style>
